e3766a2ba8d617506ea7faf39bccfd2665786764
Fanxingye <50837470+Fanxingye@users.noreply.github.com>
2021-11-03 11:35:16 +0800

    first commit

---
+++ tabular/feature_engineering/gbdt_feature.py
@@ -0,0 +1,242 @@
+import pandas as pd
+from catboost import CatBoostClassifier, CatBoostRegressor
+from lightgbm.sklearn import LGBMClassifier, LGBMRegressor
+from sklearn.base import BaseEstimator
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+from sklearn.preprocessing import OneHotEncoder
+from xgboost.sklearn import XGBClassifier, XGBRegressor
+
+__all__ = ['XGBoostFeatureTransformer',
+            'GBDTFeatureTransformer',
+            'LightGBMFeatureTransformer',
+            'CatboostFeatureTransformer',
+        ]
+
+
+class XGBoostFeatureTransformer(BaseEstimator):
+    def __init__(self,
+                 task='regression',
+                 params={
+                     'n_estimators': 100,
+                     'max_depth': 3
+                 }):
+        self.short_name = 'xbgoost'
+        if 'regression' == task:
+            self.estimator = XGBRegressor(**params)
+        else:
+            self.estimator = XGBClassifier(**params)
+
+    def fit(self, X, y, sample_weight=None):
+        """Fit estimator.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            The input samples. Use ``dtype=np.float32`` for maximum
+            efficiency. Sparse matrices are also supported, use sparse
+            ``csc_matrix`` for maximum efficiency.
+
+        y : Ignored
+            Not used, present for API consistency by convention.
+
+        sample_weight : array-like of shape (n_samples,), default=None
+            Sample weights. If None, then samples are equally weighted. Splits
+            that would create child nodes with net zero or negative weight are
+            ignored while searching for a split in each node. In the case of
+            classification, splits are also ignored if they would result in any
+            single class carrying a negative weight in either child node.
+
+        Returns
+        -------
+        self : object
+        """
+        self.fit_transform(X, y, sample_weight=sample_weight)
+        return self
+
+    def fit_transform(self, X, y=None, sample_weight=None):
+        """Fit estimator and transform dataset.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Input data used to build forests. Use ``dtype=np.float32`` for
+            maximum efficiency.
+
+        y : Ignored
+            Not used, present for API consistency by convention.
+
+        sample_weight : array-like of shape (n_samples,), default=None
+            Sample weights. If None, then samples are equally weighted. Splits
+            that would create child nodes with net zero or negative weight are
+            ignored while searching for a split in each node. In the case of
+            classification, splits are also ignored if they would result in any
+            single class carrying a negative weight in either child node.
+
+        Returns
+        -------
+        X_transformed : sparse matrix of shape (n_samples, n_out)
+            Transformed dataset.
+        """
+        self.model = self.estimator.fit(X, y, sample_weight=sample_weight)
+        self.one_hot_encoder_ = OneHotEncoder(sparse=True)
+        return self.one_hot_encoder_.fit_transform(self.model.apply(X))
+
+    def transform(self, X):
+        """Transform dataset.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Input data to be transformed. Use ``dtype=np.float32`` for maximum
+            efficiency. Sparse matrices are also supported, use sparse
+            ``csr_matrix`` for maximum efficiency.
+
+        Returns
+        -------
+        X_transformed : sparse matrix of shape (n_samples, n_out)
+            Transformed dataset.
+        """
+        return self.one_hot_encoder_.transform(self.model.apply(X))
+
+    def dense_transform(self, X, keep_original=True):
+        gbdt_leaf = self.model.apply(X)
+        onehot_embedding = self.one_hot_encoder_.transform(gbdt_leaf).toarray()
+        gbdt_feats_name = [
+            f'{self.short_name}' + '_embed_' + str(i)
+            for i in range(onehot_embedding.shape[1])
+        ]
+        gbdt_feats = pd.DataFrame(onehot_embedding, columns=gbdt_feats_name)
+        if keep_original:
+            return pd.concat([X, gbdt_feats], axis=1)
+        else:
+            return gbdt_feats
+
+
+class GBDTFeatureTransformer(BaseEstimator):
+    def __init__(self,
+                 task='regression',
+                 params={
+                     'n_estimators': 100,
+                     'max_depth': 3
+                 }):
+        self.short_name = 'GBDT'
+        if 'regression' == task:
+            self.estimator = GradientBoostingRegressor(**params)
+        else:
+            self.estimator = GradientBoostingClassifier(**params)
+
+    def fit(self, X, y, sample_weight=None):
+        self.fit_transform(X, y, sample_weight=sample_weight)
+        return self
+
+    def fit_transform(self, X, y=None, sample_weight=None):
+        self.model = self.estimator.fit(X, y, sample_weight=sample_weight)
+        self.one_hot_encoder_ = OneHotEncoder(sparse=True)
+        return self.one_hot_encoder_.fit_transform(
+            self.model.apply(X)[:, :, 0])
+
+    def transform(self, X):
+        return self.one_hot_encoder_.transform(self.model.apply(X)[:, :, 0])
+
+    def dense_transform(self, X, keep_original=True):
+        gbdt_leaf = self.model.apply(X)[:, :, 0]
+        onehot_embedding = self.one_hot_encoder_.transform(gbdt_leaf).toarray()
+        gbdt_feats_name = [
+            f'{self.short_name}' + '_embed_' + str(i)
+            for i in range(onehot_embedding.shape[1])
+        ]
+        gbdt_feats = pd.DataFrame(onehot_embedding, columns=gbdt_feats_name)
+        if keep_original:
+            return pd.concat([X, gbdt_feats], axis=1)
+        else:
+            return gbdt_feats
+
+
+class LightGBMFeatureTransformer(BaseEstimator):
+    def __init__(self,
+                 task='regression',
+                 categorical_feature='auto',
+                 params={
+                     'n_estimators': 100,
+                     'max_depth': 3
+                 }):
+        self.short_name = 'lightgbm'
+        if 'regression' == task:
+            self.estimator = LGBMRegressor(**params)
+        else:
+            self.estimator = LGBMClassifier(**params)
+
+        self.categorical_feature = categorical_feature
+
+    def fit(self, X, y):
+        self.fit_transform(X, y, categorical_feature=self.categorical_feature)
+        return self
+
+    def fit_transform(self, X, y=None, categorical_feature='auto'):
+        self.model = self.estimator.fit(X, y)
+        self.one_hot_encoder_ = OneHotEncoder(sparse=True)
+        return self.one_hot_encoder_.fit_transform(
+            self.model.predict(X, pred_leaf=True))
+
+    def transform(self, X):
+        return self.one_hot_encoder_.transform(
+            self.model.predict(X, pred_leaf=True))
+
+    def dense_transform(self, X, keep_original=True):
+        gbdt_leaf = self.model.predict(X, pred_leaf=True)
+        onehot_embedding = self.one_hot_encoder_.transform(gbdt_leaf).toarray()
+        gbdt_feats_name = [
+            f'{self.short_name}' + '_embed_' + str(i)
+            for i in range(onehot_embedding.shape[1])
+        ]
+        gbdt_feats = pd.DataFrame(onehot_embedding, columns=gbdt_feats_name)
+        if keep_original:
+            return pd.concat([X, gbdt_feats], axis=1)
+        else:
+            return gbdt_feats
+
+
+class CatboostFeatureTransformer(BaseEstimator):
+    def __init__(self,
+                 task='regression',
+                 params={
+                     'n_estimators': 100,
+                     'max_depth': 3
+                 }):
+
+        self.short_name = 'catboost'
+
+        if 'regression' == task:
+            self.estimator = CatBoostRegressor(**params,
+                                               logging_level='Silent',
+                                               allow_writing_files=False)
+        else:
+            self.estimator = CatBoostClassifier(**params,
+                                                logging_level='Silent',
+                                                allow_writing_files=False)
+
+    def fit(self, X, y, sample_weight=None):
+        self.fit_transform(X, y, sample_weight=sample_weight)
+        return self
+
+    def fit_transform(self, X, y=None, sample_weight=None):
+        self.model = self.estimator.fit(X, y, sample_weight=sample_weight)
+        self.one_hot_encoder_ = OneHotEncoder(sparse=True)
+        return self.one_hot_encoder_.fit_transform(
+            self.model.calc_leaf_indexes(X))
+
+    def transform(self, X):
+        return self.one_hot_encoder_.transform(self.model.calc_leaf_indexes(X))
+
+    def concate_transform(self, X, keep_original=True):
+        gbdt_leaf = self.model.calc_leaf_indexes(X)
+        onehot_embedding = self.one_hot_encoder_.transform(gbdt_leaf).toarray()
+        gbdt_feats_name = [
+            f'{self.short_name}' + '_embed_' + str(i)
+            for i in range(onehot_embedding.shape[1])
+        ]
+        gbdt_feats = pd.DataFrame(onehot_embedding, columns=gbdt_feats_name)
+        if keep_original:
+            return pd.concat([X, gbdt_feats], axis=1)
+        else:
+            return gbdt_feats
---
+++ tabular/feature_engineering/get_feature.py
@@ -0,0 +1,101 @@
+import pandas as pd
+from label_encoder import LabelEncoder
+from gbdt_feature import LightGBMFeatureTransformer
+from groupby_feature import get_category_columns, groupby_generate_feature
+from sklearn.feature_selection import SelectFromModel
+
+
+def get_baseline_total_data(df):
+    return pd.get_dummies(df).fillna(0)
+
+
+def get_groupby_total_data(
+        df,
+        target_name,
+        threshold=0.9,
+        k=5,
+        methods=['min', 'max', 'sum', 'mean', 'std', 'count'],
+        reserve=True):
+    generated_feature = groupby_generate_feature(df, target_name, threshold, k,
+                                                 methods, reserve)
+    return generated_feature
+
+
+def generate_cross_feature(df: pd.DataFrame, crossed_cols, keep_all=True):
+    df_cc = df.copy()
+    crossed_colnames = []
+    for cols in crossed_cols:
+        for c in cols:
+            df_cc[c] = df_cc[c].astype('str')
+        colname = '_'.join(cols)
+        df_cc[colname] = df_cc[list(cols)].apply(lambda x: '_'.join(x), axis=1)
+
+        crossed_colnames.append(colname)
+    if keep_all:
+        return df_cc
+    else:
+        return df_cc[crossed_colnames]
+
+
+def get_cross_columns(category_cols):
+    crossed_cols = []
+    for i in range(0, len(category_cols) - 1):
+        for j in range(i + 1, len(category_cols)):
+            crossed_cols.append((category_cols[i], category_cols[j]))
+    return crossed_cols
+
+
+def get_GBDT_total_data(df, target_name, task='classification'):
+    cat_col_names = get_category_columns(df, target_name)
+    label_encoder = LabelEncoder(cat_col_names)
+    total_data = label_encoder.fit_transform(df)
+    clf = LightGBMFeatureTransformer(
+        task=task,
+        categorical_feature=cat_col_names,
+        params={
+            'n_estimators': 100,
+            'max_depth': 3
+        })
+    X = total_data.drop(target_name, axis=1)
+    y = total_data[target_name]
+    clf.fit(X, y)
+    X_enc = clf.dense_transform(X, keep_original=False)
+    total_data = pd.concat([X_enc, y], axis=1)
+    return total_data
+
+
+def get_groupby_GBDT_total_data(groupby_df,
+                                target_name,
+                                task='classification',
+                                keep_original=False):
+    """Get all data after doing groupby operator and put in GBDT"""
+    cat_col_names = get_category_columns(groupby_df, target_name)
+    label_encoder = LabelEncoder(cat_col_names)
+    total_data = label_encoder.fit_transform(groupby_df)
+    clf = LightGBMFeatureTransformer(
+        task=task,
+        categorical_feature=cat_col_names,
+        params={
+            'n_estimators': 100,
+            'max_depth': 3
+        })
+    X = total_data.drop(target_name, axis=1)
+    y = total_data[target_name]
+    clf.fit(X, y)
+    X_enc = clf.dense_transform(X, keep_original=keep_original)
+    total_data = pd.concat([X_enc, y], axis=1)
+    return total_data
+
+
+def select_feature(df, target_name, estimator):
+    X = df.drop(target_name, axis=1)
+    y = df[target_name]
+    selector = SelectFromModel(estimator=estimator).fit(X, y)
+    support = selector.get_support()
+    col_names = X.columns[support]
+    X = selector.transform(X)
+    X = pd.DataFrame(X, columns=col_names)
+    total_data = X
+    total_data[target_name] = y
+    return total_data
+
